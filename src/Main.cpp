0a023b49ecde3b34bb4932073869ac481dab00cd
tooyama <toyama@dcl.cs.waseda.ac.jp>
2018-10-06 11:42:49 +0900

    [add] shot

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,7 +2,31 @@
 
 struct Bullet
 {
+    Bullet(Vec2 _pos, float _rad, float _size, ColorF _color)
+    :pos(_pos)
+    ,rad(_rad)
+    ,size(_size)
+    ,color(_color)
+    {
+        moveSpeed = Vec2(speed*Math::Sin(rad),-speed*Math::Cos(rad));
+    }
     
+    void update()
+    {
+        pos.moveBy(moveSpeed);
+    }
+    
+    void draw() const
+    {
+        Shape2D::NStar(12, size, size*0.5, pos).draw(color);
+    }
+    
+    Vec2 pos;
+    Vec2 moveSpeed;
+    ColorF color;
+    float rad;
+    float size;
+    float speed=10.0;
 };
 
 struct Virus
@@ -20,23 +44,40 @@ void Main()
     const float bulletSize = 20;
     const ColorF p1Color(ColorF(1.0,0.2,0.2));
     
-    Vec2 pos(Window::Center());
+    Vec2 p1Pos(Window::Center());
+    
+    Array<Bullet> p1Bullets;
     
     while (System::Update())
     {
         double rad = 0;
+        double deg = 0;
         
         if (const auto gamepad = Gamepad(0))
         {
             Vec2 moveRange(gamepad.axes[2],gamepad.axes[1]);
             rad = Math::Atan2(moveRange.x,-moveRange.y);
-            pos.moveBy(moveRange*speed);
+            p1Pos.moveBy(moveRange*speed);
+            deg = rad* 180.0/Math::Pi;
+            
+            if(gamepad.buttons[5].pressed())
+            {
+                p1Bullets.push_back(Bullet(p1Pos + Circular(size*0.7, deg*1.0_deg),rad,bulletSize,p1Color));
+            }
         }
         
-        double deg = rad* 180.0/Math::Pi;
+        for(auto& p1Bullet : p1Bullets)
+        {
+            p1Bullet.update();
+        }
+
+        for(const auto& p1Bullet : p1Bullets)
+        {
+            p1Bullet.draw();
+        }
         
-        Circle(pos,size*0.4).drawFrame(5,0,p1Color);
-        Circle(pos,size).drawFrame(0,5,p1Color);
-        Shape2D::NStar(12, bulletSize, bulletSize*0.5, pos + Circular(size*0.7, deg*1.0_deg)).draw(p1Color);
+        Circle(p1Pos,size*0.4).drawFrame(5,0,p1Color);
+        Circle(p1Pos,size).drawFrame(0,5,p1Color);
+        Shape2D::NStar(12, bulletSize, bulletSize*0.5, p1Pos + Circular(size*0.7, deg*1.0_deg)).draw(p1Color);
     }
 }
